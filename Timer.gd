389b86ed01ecb2442ae1f999f2775a99adfd69ae
Mike <mikewong.oona@gmail.com>
2019-10-30 17:52:17 +0800

    Added testing for custom signal.

--- Timer.gd
+++ Timer.gd
@@ -1,5 +1,5 @@
 extends Timer
-
+signal my_signal
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -16,7 +16,15 @@ func _ready():
 #func _process(delta):
 #	pass
 
-
 func _on_Timer_timeout():
 		var s = get_node('../SpriteTest')
 		s.visible = !s.visible
+		emit_signal("my_signal")
+
+
+func _on_Timer_my_signal():
+#	The custom signal 'my_signal' is connected through editor. May test 
+#	connection through code.
+	var s = get_node('../SpriteTest2')
+	s.visible = !s.visible
+	pass # Replace with function body.
