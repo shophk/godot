26182ddbcc9b06a0c3527872b4e8a55cdbe37bfb
Mike <mike@>
2019-11-19 23:46:53 +0800

    Added change position of player object by pressing arrow keys.

--- DodgeGame/Player/Player.gd
+++ DodgeGame/Player/Player.gd
@@ -13,17 +13,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    var velocity = Vector2()  # The player's movement vector.
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * speed
-        $AnimatedSprite.play()
-    else:
-        $AnimatedSprite.stop()
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		print(velocity)
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
