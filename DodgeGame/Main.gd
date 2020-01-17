193557bb550a3d49eaa9727391002e820a21adcb
Mike <Wong>
2020-01-17 22:07:24 +0800

    Added HUD

--- DodgeGame/Main.gd
+++ DodgeGame/Main.gd
@@ -8,7 +8,7 @@ var score
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	new_game()
+	#new_game()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
