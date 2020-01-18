d024d46ab81fba89c717e789de2c7aca1b4ef11e
Mike <Wong>
2020-01-18 17:37:47 +0800

    Hooked up HUD to main.

--- DodgeGame/Main.gd
+++ DodgeGame/Main.gd
@@ -19,11 +19,14 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
-    score = 0
-    $Player.start($StartPosition.position)
-    $StartTimer.start()
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_StartTimer_timeout():
@@ -36,18 +39,22 @@ func _on_ScoreTimer_timeout():
 
 
 func _on_MobTimer_timeout():
-    # Choose a random location on Path2D.
-    $MobPath/MobSpawnLocation.set_offset(randi())
-    # Create a Mob instance and add it to the scene.
-    var mob = Mob.instance()
-    add_child(mob)
-    # Set the mob's direction perpendicular to the path direction.
-    var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
-    # Set the mob's position to a random location.
-    mob.position = $MobPath/MobSpawnLocation.position
-    # Add some randomness to the direction.
-    direction += rand_range(-PI / 4, PI / 4)
-    mob.rotation = direction
-    # Set the velocity (speed & direction).
-    mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
-    mob.linear_velocity = mob.linear_velocity.rotated(direction)
+	# Choose a random location on Path2D.
+	$MobPath/MobSpawnLocation.set_offset(randi())
+	# Create a Mob instance and add it to the scene.
+	var mob = Mob.instance()
+	add_child(mob)
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+	# Set the mob's position to a random location.
+	mob.position = $MobPath/MobSpawnLocation.position
+	# Add some randomness to the direction.
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	# Set the velocity (speed & direction).
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+	
+	#This line tells the new Mob node (referenced by the mob variable) to 
+	#respond to any game_reset signal emitted by the HUD node by running its _on_start_game() function.
+	$HUD.connect("game_reset", mob, "_on_start_game")
--- DodgeGame/Mob/Mob.gd
+++ DodgeGame/Mob/Mob.gd
@@ -15,7 +15,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
+func _on_start_game():
+    queue_free()
 
 func _on_Visibility_screen_exited():
 	queue_free()
